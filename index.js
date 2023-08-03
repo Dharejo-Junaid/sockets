01bb45ae0982bda9c287f6e46d494450c918e5ee
Dharejo-Junaid <junaidali.100190@gmail.com>
2023-08-03 19:16:21 +0500

    add some styling

--- index.js
+++ index.js
@@ -16,14 +16,17 @@ io.on("connection", (socket) => {
   console.log("User is connected");
 
   socket.on("join", name => {
-    if(!name) name = "unknown";
     users[socket.id] = name;
-    socket.broadcast.emit("messageBack", `${name} joined the chat`);
+    socket.broadcast.emit("messageBack", `${name} joined the chat...`);
   });
 
   socket.on("message", msg => {
     console.log("User msg = ", msg);
     socket.broadcast.emit("messageBack", `${users[socket.id]} : ${msg}`);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("logout", `${users[socket.id]} left the chat...`);
   });
 });
 
